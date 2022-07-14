8f9fc6f7519907e2c7e2bd70cd52e5769ac1b3e9
tanya1524 <tanyabharti1524@gmail.com>
2022-07-14 15:39:48 +0530

    update

---
+++ index.js
@@ -0,0 +1,159 @@
+//alert("hello world");
+//console.log("hello world");
+//console.error("hello world");
+
+//Variable
+
+
+//var x = 3;
+//var y = 5;
+//var z = 15;
+
+//console.log(x);
+//console.log(y);
+//console.log(z);
+
+//Data types
+
+//1.Number
+//let x = 3;
+
+//2.String
+//let str1 ="Mango"
+
+//3.Boolean
+//let a = true;
+//let b = false;
+
+//console.log(a);
+//console.log(b);
+
+//4.Undefined
+//let x;
+//console.log(x);
+
+//.Null
+//let y = null;
+//console.log(y);
+
+//1.Primitive data type
+//2.Refrence data type : Array,Objects.
+
+//(i)Objects
+//let marks={ram:34,shyam;55,mohit:45};
+//console.log(marks);
+//console.log(marks[ram]);
+
+//(ii)Array
+//let arr=[1,"ram",[1,2,4,5]];
+//console.log("this array",arr);
+//console.log(arr[1]);
+
+//Conditionals
+//let age = 16;
+//if(age>18){
+   // console.log("you are young");
+//}
+//else if(age=18){
+   // console.log("you can drive");
+//}
+//else if(age<18){
+    //console.log("you are kid");
+//};
+//operator
+//1.arithematic operator
+//let a= 5;
+//let b= 6;
+
+//console.log(a+b);
+//console.log(a-b);
+//console.log(a*b);
+//console.log(a/b);
+
+//2.assignment operator
+//let c=a;
+//c+=2;
+//console.log(c);
+
+ //3.logical operator
+//console.log(true||false)
+//console.log(true||false)
+//console.log(true||false)
+//console.log(true||false)
+
+//console.log(true&&false)
+//console.log(true&&false)
+//console.log(true&&false)
+//console.log(true&&false)
+
+//switch
+//let age=15;
+//switch(true){
+   // case(age>=18 && age<=55);
+   //document.write()
+//}
+
+//case(age>=18 && age<=55)
+
+
+
+
+//Method of array
+//let myArr =["ram","potato",108,null,true];
+//console.log(myArr.length)
+//myArr.pop()
+//myArr.push("cat")
+//myArr.shift()
+//myArr.unshift("shyam")
+//console.log(myArr)
+
+//string method in javascript
+//let myString ="ram is a good boy"
+//console.log(myString.length);
+
+//console.log(myString.indexOf("good"))
+
+//console.log(myString.slice(0,4))
+
+ //d=myString.replace("ram","shyam")
+ //d=myString.replace("good","bad")
+ //console.log(d)
+
+ //loops in javascript
+ //for loops
+// let arr =[1,2,4,5,6,7]
+
+// console.log(arr)
+ //for(let i= 0; i<arr.length;i++){
+    //if(i==2){
+        //continue;
+//}
+    // console.log(arr[i]);
+//}
+
+//let j= 0;
+//while(j<arr.length){
+    //console.log(arr[j]);
+    //j++
+//}
+
+//Function
+//DRY = do not repeat yourself
+//function avg(a,b){
+    //return(a+b)/2;
+//}
+  //c1 = avg(12,12)
+  //console.log(c1)
+
+  //or
+  avg =(a,b) =>{
+    c=(a+b)/2;
+    return c;
+  }
+  c1= avg(4,6);
+  console.log(c1);
+  
+ 
+
+
+
